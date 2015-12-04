3ede8c5c2e7c98c9c992791d4c6f822c2606e014
n-martin <nicolas.g.martin@gmail.com>
2015-12-04 16:37:15 -0500

    added multi-level parallelization

--- minimaxAIParallel.py
+++ minimaxAIParallel.py
@@ -12,6 +12,7 @@ opponentPlayer = -1
 maxTime = 15
 startTime = 0
 maxDepth = 8
+numCPU = 0
 
 # returns an action
 def alpha_beta_search(board):
@@ -26,12 +27,44 @@ def max_value_first(board, alpha, beta, depth):
     v = float('-inf')
     moves = mmUtil.get_moves(board)
     
+    # Get number of extra processors after first level
+    num_extra = numCPU - len(moves)
+    # Ensure num_extra is positive
+    if num_extra < 0:
+        num_extra = 0
+
     pool = multiprocessing.Pool()
     func = partial(t.find_min, board, activePlayer, alpha, beta, depth)
-    v_mins = pool.map_async(func, moves)
+
+    # Make array of tuples for each node of tree at the next depth
+    # each tuple: (move, # of moves available)
+    next_nodes = []
+    for move in moves:
+        temp_board = mmUtil.make_move(board, move, activePlayer)
+        num_next = mmUtil.get_moves(temp_board)
+        next_nodes.append((move, len(num_next)))
+
+    # Sort by decreasing number of moves available
+    sorted(next_nodes, key=itemgetter(1), reverse=True)
+
+    # Create data array for number of processors allocated for each move
+    data = []
+
+    # Calculate the base number of extra processors that each move will use
+    base = num_extra/7
+
+    # For each tuple add the base number of extra processors to the default amount (1)
+    for node in next_nodes:
+        data.append((node[0], 1+base))
+
+    # Allocate remaining processor with moves that have the highest branching factor
+    for i in range((num_extra % 7)):
+        node = data[i]
+        data[i] = (node[0], node[1]+1)
+
+    v_mins = pool.map_async(func, data)
     pool.close()
     pool.join()
-
     mov_val = max(v_mins.get(), key=itemgetter(1)) # (move, value)
 
     print v_mins.get()
@@ -65,10 +98,12 @@ def min_value(board, alpha, beta, depth):
 def run_AI(board, ID):
     global activePlayer
     global opponentPlayer
+    global numCPU
     activePlayer = ID
     if activePlayer == 0:
         opponentPlayer = 1
     else:
         opponentPlayer = 0
+    numCPU = multiprocessing.cpu_count()
     v = alpha_beta_search(board)
     return str(v)
--- my_thread.py
+++ my_thread.py
@@ -1,6 +1,24 @@
 import minimaxAIParallel as ai
 import minimaxCommon as mmUtil
 
-def find_min(board,activePlayer,alpha,beta,depth,a):
+def find_min(board, activePlayer, alpha, beta, depth, mov_cpu):
+	print "Data for move " + str(mov_cpu[0]) + ": " + str(mov_cpu)
+	a = mov_cpu[0]
+	numCPU = mov_cpu[1]
 
-	return (a, ai.min_value(mmUtil.make_move(board,a,activePlayer), alpha, beta, depth+1))
+	if numCPU == 1:
+		return (a, ai.min_value(mmUtil.make_move(board,a,activePlayer), alpha, beta, depth+1))
+	else:
+		moves = mmUtil.get_moves(board)
+		data = []
+		for move in moves:
+			data.append((move, 1))
+
+		pool = multiprocessing.Pool(processes = numCPU)
+
+		func = partial(find_min, board, activePlayer, alpha, beta, depth+1)
+		v_mins = pool.map_async(func, data)
+		pool.close()
+		pool.join()
+		mov_val = max(v_mins.get(), key=itemgetter(1)) # (move, value)
+		return mov_val[::-1]
